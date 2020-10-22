7da03bbe6ecf62671f43ffc3ca089e63c1b1561d
alley.ng.com@gmail.com <alley.ng2020>
2020-10-22 09:21:08 +0100

    Initial commit

---
+++ lib/data/chat_item_model.dart
@@ -0,0 +1,47 @@
+class ChatItemModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatItemModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatItemModel> mockData = [
+  new ChatItemModel(
+      name: "Mike Kemi",
+      message: "Welcome to Codesource.io",
+      time: "15:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatItemModel(
+      name: "Mike Mike",
+      message: "Welcome to Codesource.io",
+      time: "17:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatItemModel(
+      name: "Mike Kemi",
+      message: "Hello from Codesource.io",
+      time: "5:00",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatItemModel(
+      name: "Deven",
+      message: "Hi there",
+      time: "10:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatItemModel(
+      name: "Flutter",
+      message: "Hey there",
+      time: "12:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+  new ChatItemModel(
+      name: "Mark Zuckerberg",
+      message: "I am the CEO of Facebook",
+      time: "15:30",
+      avatarUrl:
+      "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb"),
+];
